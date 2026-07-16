5cc05bb4163173f6f67dec1c6b8112d3dd922889
Hamzah Shahbaz Khan <khanhamzah142@gmail.com>
2026-07-16 23:17:17 +0530

    add Hand struct and its basic method

--- crates/bj-core/src/hand.rs
+++ crates/bj-core/src/hand.rs
@@ -1,3 +1,4 @@
+use crate::card::Card;
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct HandValue {
     pub hard: u8,
@@ -24,5 +25,43 @@ impl HandValue {
     }
     pub fn is_twenty_one(self) -> bool {
         self.best() == 21
+    }
+}
+
+pub struct Hand {
+    cards: Vec<Card>,
+    pub bet: u32,
+}
+
+impl Hand {
+    pub fn new(bet: u32) -> Self {
+        Self {
+            cards: Vec::with_capacity(8),
+            bet,
+        }
+    }
+    pub fn add_cards(&mut self, card: Card) {
+        self.cards.push(card);
+    }
+    pub fn cards(&self) -> &[Card] {
+        &self.cards
+    }
+    // Above we are returning &[Card] because it is more idiomatic. The caller dosen't need to know
+    // that cards is a Vec
+    pub fn len(&self) -> usize {
+        self.cards.len()
+    }
+    pub fn is_empty(&self) -> bool {
+        self.cards.is_empty()
+    }
+    pub fn first(&self) -> Option<Card> {
+        self.cards.first().copied()
+    }
+    // pub fn value()
+    pub fn is_natural(&self) -> bool {
+        self.cards.len() == 2 && self.value().best() == 21
+    }
+    pub fn is_pair(&self) -> bool {
+        self.cards.len() == 2 && self.cards[0].rank == self.cards[1].rank
     }
 }
